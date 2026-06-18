24f0e9efa58c42b6df0e8a1db03f90fbee4bb957
aleconstancio <alexandreconstancio14@gmail.com>
2026-06-18 09:40:42 -0300

    feat: add BlogPage template

---
+++ src/components/boundrune/marketing/BlogPage.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+import type { Snippet } from "svelte";
+import DashboardShellHeader from "../../dashboard/DashboardShellHeader.svelte";
+import PageShell from "../../PageShell.svelte";
+
+let {
+	title = "Blog",
+	class: className = "",
+	sidebar,
+	header,
+	children,
+}: {
+	title?: string;
+	class?: string;
+	sidebar?: Snippet;
+	header?: Snippet;
+	children?: Snippet;
+} = $props();
+</script>
+
+{#snippet topbar()}
+	<DashboardShellHeader resolvedTitle={title} {header} />
+{/snippet}
+
+{#snippet leftPanel()}
+	{#if sidebar}
+		<div class="p-4">
+			{@render sidebar()}
+		</div>
+	{/if}
+{/snippet}
+
+<PageShell
+	topbar={topbar}
+	left={sidebar ? leftPanel : undefined}
+	leftWidth="280px"
+	leftCollapsible="icon"
+	class={className}
+>
+	{#snippet main()}
+		<div class="p-6">
+			{@render children?.()}
+		</div>
+	{/snippet}
+</PageShell>
