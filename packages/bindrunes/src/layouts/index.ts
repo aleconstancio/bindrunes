6c2c6c8633c5f406e9784fa5755b948602e6fd69
aleconstancio <alexandreconstancio14@gmail.com>
2026-06-19 22:10:55 -0300

    feat: create layouts index

---
+++ packages/bindrunes/src/layouts/index.ts
@@ -0,0 +1,41 @@
+// Layout components
+
+export { default as DynamicIcon } from "./DynamicIcon.svelte";
+// Dashboard shell components
+export { default as DashboardShell } from "./dashboard/DashboardShell.svelte";
+export { default as DashboardShellBrand } from "./dashboard/DashboardShellBrand.svelte";
+export { default as DashboardShellHeader } from "./dashboard/DashboardShellHeader.svelte";
+export { default as DashboardShellRight } from "./dashboard/DashboardShellRight.svelte";
+export { default as DashboardShellSplit } from "./dashboard/DashboardShellSplit.svelte";
+export { default as DashboardShellTopnav } from "./dashboard/DashboardShellTopnav.svelte";
+export { default as NavMenu } from "./dashboard/NavMenu.svelte";
+export { default as ErrorBoundary } from "./ErrorBoundary.svelte";
+export { default as LazyLoad } from "./LazyLoad.svelte";
+export { default as ListPage } from "./ListPage.svelte";
+export { default as MetaContainer } from "./MetaContainer.svelte";
+export { default as MetaLayout } from "./MetaLayout.svelte";
+export { default as MetaScrollable } from "./MetaScrollable.svelte";
+export { default as PageHeader } from "./PageHeader.svelte";
+export { default as PageSection } from "./PageSection.svelte";
+export { default as PageShell } from "./PageShell.svelte";
+export { default as SEO } from "./SEO.svelte";
+export { default as SectionHeader } from "./SectionHeader.svelte";
+
+// Sidebar components
+export {
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarGroup,
+	SidebarHeader,
+	SidebarLayout,
+	SidebarMenu,
+	SidebarMenuBadge,
+	SidebarMenuButton,
+	SidebarMenuItem,
+	SidebarMenuSkeleton,
+	SidebarProvider,
+	SidebarRail,
+	SidebarSeparator,
+	SidebarTrigger,
+} from "./sidebar/index.ts";
