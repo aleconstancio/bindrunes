56e1289e0c055e84f400085df90032e73d2bd9f9
aleconstancio <alexandreconstancio14@gmail.com>
2026-06-26 02:54:19 -0300

    feat: export all playground components and utilities

--- packages/bindrunes/src/playground/index.ts
+++ packages/bindrunes/src/playground/index.ts
@@ -1,6 +1,21 @@
+// packages/bindrunes/src/playground/index.ts
+
+export { default as CodePreview } from "./CodePreview.svelte";
+export type { ComponentDefinition, PropDefinition } from "./component-registry";
+export {
+	categories,
+	componentRegistry,
+	getComponentsByCategory,
+	searchComponents,
+} from "./component-registry";
 export { default as DemoFooter } from "./DemoFooter.svelte";
 export { default as DemoLayout } from "./DemoLayout.svelte";
 export { default as DemoNav } from "./DemoNav.svelte";
+export { default as ExportButton } from "./ExportButton.svelte";
+export { default as Playground } from "./Playground.svelte";
+export { default as PropControls } from "./PropControls.svelte";
+export { createPlaygroundState } from "./playground-state.svelte";
+export { default as ResponsiveFrame } from "./ResponsiveFrame.svelte";
 export type { DemoState } from "./scaffold-context.svelte";
 export { createDemoState, useDemo } from "./scaffold-context.svelte";
 export type {
